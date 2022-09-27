6dc501eb5e54433ba918b2723c94564f876ff5e6
Burak Mert Taş <bt1111.tas@gmail.com>
2022-09-27 14:15:05 +0300

    initial commit

---
+++ Assets/Scrpts/Managers/MainMenuPanel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Scripts.Managers;
+public class MainMenuPanel : UIPanel
+{
+    public override void Initialize(UIManager uiManager)
+    {
+        base.Initialize(uiManager);
+        Debug.Log("initializedddd!!!!");
+    }
+}
---
+++ Assets/Scrpts/Managers/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.UI;
+
+namespace Scripts.Managers
+{
+
+    public class UIManager : CustomBehaviour
+    {
+        public MainMenuPanel MainMenuPanel => _mainMenuPanel;
+        [SerializeField] private MainMenuPanel _mainMenuPanel;
+
+        private List<UIPanel> UIPanels;
+
+        public override void Initialize(GameManager gameManager)
+        {
+            base.Initialize(gameManager);
+            UIPanels = new List<UIPanel> { _mainMenuPanel };
+
+            UIPanels.ForEach(x =>
+            {
+                x.Initialize(this);
+                x.gameObject.SetActive(false);
+            });
+            //hudPanel.ShowPanel();
+
+        }
+    }
+}
