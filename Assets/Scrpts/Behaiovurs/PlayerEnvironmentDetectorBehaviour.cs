5367c98fcb51c83ddf4f10b48ef0c5c512cad049
Burak Mert Taş <bt1111.tas@gmail.com>
2022-09-29 23:58:43 +0300

    deleting unneccessery lines

--- Assets/Scrpts/Behaiovurs/PlayerEnvironmentDetectorBehaviour.cs
+++ Assets/Scrpts/Behaiovurs/PlayerEnvironmentDetectorBehaviour.cs
@@ -11,11 +11,6 @@ namespace Scripts.Behaviours
     public class PlayerEnvironmentDetectorBehaviour : MonoBehaviour
     {
         private PlayerController _playerController;
-        private Coroutine _countdownSpeedBoostCo;
-
-        private float _pathDistance;
-        private CinemachineSmoothPath _currentPath;
-        private bool _isPathingActive;
         public void Initialize(PlayerController playerController)
         {
             _playerController = playerController;
@@ -27,50 +22,6 @@ namespace Scripts.Behaviours
             {
                 other.GetComponent<IInteract>().Interact(_playerController.GameManager);
             }
-
-
-            if (other.gameObject.CompareTag("Obstacle"))
-            {
-                //if (!_playerController.GodMode)
-                //{
-                //    _playerController.PlayerMovementBehaviour.Speed = 0f;
-                //    _playerController.PlayerAnimationController.SetAnimation(0, true);
-                    
-                //}
-                //Interface Done..
-            }
-
-            if (other.gameObject.CompareTag("SpeedBoost"))
-            {
-                //_playerController.GameManager.EventManager.GodModCombo();
-                //_playerController.PlayerMovementBehaviour.Speed = 8f;
-                //Interface Done..
-            }
-
-            if (other.gameObject.CompareTag("RampBoost"))
-            {
-                //InputController.IsInputDeactivated = true;
-
-                //_currentPath =  other.gameObject.GetComponent<PathBehaviour>().Path;
-                //_isPathingActive = true;
-
-                //Interface Done..
-                
-            }
-
-            //if (other.gameObject.CompareTag("SuffleGate"))
-            //{
-            //    //if (other.gameObject.GetComponent<SuffleGateBehaviour>().IsRandomSuffleGate)
-            //    //{
-            //    //    _playerController.GameManager.EventManager.BlocksShuffledRandomly();
-            //    //}
-            //    //else
-            //    //{
-            //    //    _playerController.GameManager.EventManager.BlocksShuffledByColors();
-            //    //}
-
-            //    //Interface Done..
-            //}
         }
 
     }
